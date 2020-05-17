f4344feadcf51df02fc09d80acc399b3323072c2
Ilmira-Gimadeeva <Ilmira.Gimadeeva@appline.ru>
2020-05-17 23:52:09 +0300

    Выполнена итоговая задача №1

--- src/main/java/BinaryFormat.java
+++ src/main/java/BinaryFormat.java
@@ -8,9 +8,16 @@ public class BinaryFormat {
         String s = in.nextLine();
         int num = 0;
         int position = 0;
+
         for (int i = s.length() - 1; i >= 0; i--) {
-            num += Character.getNumericValue(s.charAt(position)) * Math.pow(2, i);
-            position++;
+            int value = Character.getNumericValue(s.charAt(position));
+            if (value < 2) {
+                num += Character.getNumericValue(s.charAt(position)) * Math.pow(2, i);
+                position++;
+            } else {
+                System.out.println("Вы ввели не двоичное число!");
+                return;
+            }
         }
         System.out.print("Десятичное представление: " + num);
     }
