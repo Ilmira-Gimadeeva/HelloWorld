fd36c60275f886b01ae255b74dcb71a1c09775c5
Ilmira-Gimadeeva <Ilmira.Gimadeeva@appline.ru>
2021-04-13 23:01:33 +0300

    Выполнено задание №3 (2021)

---
+++ src/main/java/MaximumElementArray.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class MaximumElementArray {
+    public static void main(String[] args) {
+        searchMax();
+    }
+
+    public static void searchMax() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите длину массива:");
+        int arrLength = scanner.nextInt();
+
+        if (arrLength <= 0) {
+            System.out.println("Задана отрицательная или нулевая длина массива");
+            return;
+        }
+
+        System.out.println("Введите слова:");
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        if (a.length() == 0) {
+            System.out.println("Вы ничего не ввели");
+            return;
+        }
+
+        String[] array = a.split(" ");
+
+        if (array.length > arrLength) {
+            System.out.println("Введенное количество слов превышает длину массива");
+            return;
+        }
+
+        String max = "";
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length() > max.length()) {
+                max = array[i];
+            }
+        }
+        System.out.println("Самое длинное слово: " + max);
+    }
+}
---
+++ src/main/java/TaskOfChoice.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TaskOfChoice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите операцию:\n" + "1 - запуститься выполнение калькулятора\n" + "2 - поиск максимального слова в массиве");
+
+        char a = scanner.next().charAt(0);
+        switch (a) {
+            case '1':
+                Calculator.calculate();
+                break;
+            case '2':
+                MaximumElementArray.searchMax();
+                break;
+        }
+    }
+}
